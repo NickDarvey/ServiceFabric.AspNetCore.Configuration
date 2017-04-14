b212ab2f1ee15c0c4e49ce7e42beda96ced5d62e
Nick Darvey <nick@two-bulls.com>
2017-04-14 16:17:39 +1000

    Initital commit

---
+++ SomethingSomethingConfig/ServiceFabricConfigurationExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomethingSomethingConfig
+{
+    public static class ServiceFabricConfigurationExtensions
+    {
+        public static IConfigurationBuilder AddServiceFabricConfiguration(this IConfigurationBuilder builder, ServiceContext ctx, string configPackageObjectName)
+        {
+            builder.Add(new ServiceFabricConfigurationSource(ctx, configPackageObjectName));
+            return builder;
+        }
+    }
+}
---
+++ SomethingSomethingConfig/ServiceFabricConfigurationProvider.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomethingSomethingConfig
+{
+    internal class ServiceFabricConfigurationProvider : ConfigurationProvider
+    {
+        private readonly Func<ConfigurationPackage> _getConfig;
+
+        public ServiceFabricConfigurationProvider(ServiceContext ctx, string configPackageObjectName)
+        {
+            _getConfig = () => ctx.CodePackageActivationContext.GetConfigurationPackageObject(configPackageObjectName);
+            ctx.CodePackageActivationContext.ConfigurationPackageModifiedEvent += (sender, e) =>
+            {
+                Reload(e.NewPackage);
+                OnReload();
+            };
+        }
+
+        public override void Load() => Reload(_getConfig());
+
+        private void Reload(ConfigurationPackage pkg)
+        {
+            Data.Clear();
+
+            var configs = pkg
+                .Settings.Sections
+                .SelectMany(section => section.Parameters
+                .Select(param => (Key: $"{section.Name}{ConfigurationPath.KeyDelimiter}{param.Name}", Value: param.Value)));
+
+            foreach (var config in configs) { Data[config.Key] = config.Value; }
+        }
+    }
+}
